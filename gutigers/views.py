7f64f3dda26b95d0836f40008ac7ef3f605e363c
Apurba Darshan Upreti <upretitristan@gmail.com>
2023-02-26 15:45:52 +0000

    extremely primitive implementation of the views and url mapping, implementation works

--- gutigers/views.py
+++ gutigers/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("Initial testing of the site")
+    return render(request, 'gutigers/index.html')
+    
+def team(request):
+    return render(request, 'gutigers/team.html')
+    
+def contact(request):
+    return render(request, 'gutigers/contact.html')
+
+def player(request):
+    return render(request, 'gutigers/player.html')
+
+def team(request):
+    return render(request, 'gutigers/team.html')
+    
+def post(request):
+    return render(request, 'gutigers/post.html')
+    
+def login(request):
+    return render(request, 'gutigers/login.html')
+    
+def register(request):
+    return render(request, 'gutigers/register.html')
+    
+def result(request):
+    return render(request, 'gutigers/result.html')
+    
+def user(request):
+    return render(request, 'gutigers/user.html')
--- gutigers_project/settings.py
+++ gutigers_project/settings.py
@@ -15,6 +15,9 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+#RELATIVE PATH TO THE TEMPLATE DIRECTORY
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'gutigers_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR, ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
