ed6a816a3044e61ed6400a60066232aaddd10601
WylieYYYY <wylieyyyy@gmail.com>
2023-03-03 16:04:24 +0000

    Added bootstraps and styles for comments

--- gutigers/helpers/comment.py
+++ gutigers/helpers/comment.py
@@ -2,23 +2,20 @@ from gutigers.models import Comment, Team, UserProfile
 
 class CommentView:
     def __init__(self, comment_orm: Comment): self.comment_orm = comment_orm
-    @property
     def children(self):
         return list(map(CommentView, Comment.objects.filter(replies_to=self.comment_orm)))
-    @property
     def author(self): return UserView(self.comment_orm.user)
-    @property
     def rating(self): return self.comment_orm.rating
-    @property
+    def rating_color(self):
+        if self.comment_orm.rating > 0: return 'green'
+        elif self.comment_orm.rating < 0: return 'red'
+        else: return 'gray'
     def body(self): return self.comment_orm.body
 
 class UserView:
     def __init__(self, user_orm: UserProfile): self.user_orm = user_orm
-    @property
     def url_slug(self): return self.user_orm.url_slug
-    @property
     def name(self): return self.user_orm.display_name
-    @property
     def faction(self):
         if self.user_orm.work_team is not None:
             return Faction(self.user_orm.work_team, 'Member')
