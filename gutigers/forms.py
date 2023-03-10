093b5e9410c48ba33aca203ac6c24d4613b52a01
Fionn Blaney <github@fionnblaney.co.uk>
2023-03-10 15:34:14 +0000

    Added login and registration functionality and templates

--- gutigers/forms.py
+++ gutigers/forms.py
@@ -1,5 +1,7 @@
 from django import forms
-from gutigers.models import Comment
+from gutigers.models import Comment, UserProfile
+from django.contrib.auth.models import User
+from gutigers.models import Team
 
 class CommentForm(forms.ModelForm):
     body = forms.CharField(widget=forms.Textarea(attrs={'rows': '3'}), help_text='Reply:')
@@ -8,3 +10,17 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body', 'rating')
+      
+TEAM_CHOICES = list(map(lambda team: (team.pk, team.name), Team.objects.all()))
+        
+class UserForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput())
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+class UserProfileForm(forms.ModelForm):
+    team_supported = forms.MultipleChoiceField(choices = TEAM_CHOICES)
+    class Meta:
+        model = UserProfile
+        fields = ('avatar', 'display_name', 'team_supported')
