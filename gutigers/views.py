e37fd6b835902c303caae7eab0c323dc36cf23d1
Mark Strathie <2147105s@student.gla.ac.uk>
2023-03-24 12:08:24 +0000

    Big style fix

--- gutigers/views.py
+++ gutigers/views.py
@@ -29,6 +29,7 @@ def team_detail(request, *, team_name_slug):
     context_dict['supporter_count'] = (UserProfile.objects
                                        .filter(support_team=context_dict['team']).count())
     if team_name_slug == 'gutigers':
+        context_dict['upper_half'] = True
         context_dict['posts'] = Post.objects.order_by('-post_date')
         context_dict['post_id'] = -1
         context_dict['comments'] = list(map(CommentView,
